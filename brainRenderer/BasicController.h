b9996767851a9256379a3d5fbb1bee18a2eb0377
Seb James <seb@opteran.com>
2024-05-01 13:07:18 +0100

    Use bitset for move state

--- brainRenderer/BasicController.h
+++ brainRenderer/BasicController.h
@@ -2,8 +2,26 @@
 
 #include <sutil/vec_math.h>
 #include <GLFW/glfw3.h>
+#include <bitset>
 
 // A basic system for camera movement
+
+enum class move_sense
+{
+    forward,
+    backward,
+    left,
+    right,
+    up,
+    down,
+    rotUp,
+    rotDown,
+    rotLeft,
+    rotRight,
+    zoomIn,
+    zoomOut
+};
+
 class BasicController
 {
 public:
@@ -11,41 +29,31 @@ public:
     float angularSpeed = 4.0f * M_PIf * 0.5f/180;
 
     // Returns true if an update is made
-    bool ingestKeyAction(int32_t key, int32_t action)
+    bool ingestKeyAction (int32_t key, int32_t action)
     {
         bool output = false;
-
+        std::bitset<12> mvst_cpy = this->move_state;
         if (action == GLFW_PRESS || action == GLFW_REPEAT) {
             if (key == GLFW_KEY_W) {
-                output |= !forward;
-                forward = true;
+                this->move_state.set (static_cast<int>(move_sense::forward));
             } else if (key == GLFW_KEY_A) {
-                output |= !left;
-                left = true;
+                this->move_state.set (static_cast<int>(move_sense::left));
             } else if (key == GLFW_KEY_D) {
-                output |= !right;
-                right = true;
+                this->move_state.set (static_cast<int>(move_sense::right));
             } else if (key == GLFW_KEY_S) {
-                output |= !backward;
-                backward = true;
+                this->move_state.set (static_cast<int>(move_sense::backward));
             } else if (key == GLFW_KEY_P) {
-                output |= !up;
-                up = true;
+                this->move_state.set (static_cast<int>(move_sense::up));
             } else if (key == GLFW_KEY_L) {
-                output |= !down;
-                down = true;
+                this->move_state.set (static_cast<int>(move_sense::down));
             } else if (key == GLFW_KEY_UP) {
-                output |= !rotUp;
-                rotUp = true;
+                this->move_state.set (static_cast<int>(move_sense::rotUp));
             } else if (key == GLFW_KEY_DOWN) {
-                output |= !rotDown;
-                rotDown = true;
+                this->move_state.set (static_cast<int>(move_sense::rotDown));
             } else if (key == GLFW_KEY_LEFT) {
-                output |= !rotLeft;
-                rotLeft = true;
+                this->move_state.set (static_cast<int>(move_sense::rotLeft));
             } else if (key == GLFW_KEY_RIGHT) {
-                output |= !rotRight;
-                rotRight = true;
+                this->move_state.set (static_cast<int>(move_sense::rotRight));
             } else if (key == GLFW_KEY_END) {
                 speed = speed * 0.5f;
                 angularSpeed = angularSpeed * 0.5f;
@@ -56,26 +64,50 @@ public:
                 std::cout << "Speed increased to " << speed << std::endl;
             }
         }
-        return output;
+        return (this->move_state ^ mvst_cpy).any();
     }
 
     float3 getMovementVector()
     {
         float3 output = make_float3(0.0f, 0.0f, 0.0f);
-        if(up) { output += speed*UP; up = false; }
-        if(down) { output += speed*DOWN; down = false; }
-        if(left) { output += speed*LEFT; left = false; }
-        if(right) { output += speed*RIGHT; right = false; }
-        if(forward) { output += speed*FORWARD; forward = false; }
-        if(backward) { output += speed*BACK; backward = false; }
+        if (this->move_state.test(static_cast<int>(move_sense::up))) {
+            output += speed*UP;
+            this->move_state.set(static_cast<int>(move_sense::up), false);
+        }
+        if (this->move_state.test(static_cast<int>(move_sense::down))) {
+            output += speed*DOWN;
+            this->move_state.set(static_cast<int>(move_sense::down), false);
+        }
+        if (this->move_state.test(static_cast<int>(move_sense::left))) {
+            output += speed*LEFT;
+            this->move_state.set(static_cast<int>(move_sense::left), false);
+        }
+        if (this->move_state.test(static_cast<int>(move_sense::right))) {
+            output += speed*RIGHT;
+            this->move_state.set(static_cast<int>(move_sense::right), false);
+        }
+        if (this->move_state.test(static_cast<int>(move_sense::forward))) {
+            output += speed*FORWARD;
+            this->move_state.set(static_cast<int>(move_sense::forward), false);
+        }
+        if (this->move_state.test(static_cast<int>(move_sense::backward))) {
+            output += speed*BACK;
+            this->move_state.set(static_cast<int>(move_sense::backward), false);
+        }
         return output;
     }
 
     float getVerticalRotationAngle()
     {
         float out = 0.0f;
-        if(rotUp) { out += angularSpeed; rotUp = false; }
-        if(rotDown) { out -= angularSpeed; rotDown = false; }
+        if (this->move_state.test(static_cast<int>(move_sense::rotUp))) {
+            out += angularSpeed;
+            this->move_state.set(static_cast<int>(move_sense::rotUp), false);
+        }
+        if (this->move_state.test(static_cast<int>(move_sense::rotDown))) {
+            out -= angularSpeed;
+            this->move_state.set(static_cast<int>(move_sense::rotDown), false);
+        }
         return out;
     }
 
@@ -83,18 +115,19 @@ public:
     float getHorizontalRotationAngle()
     {
         float out = 0.0f;
-        if(rotLeft) { out += angularSpeed; rotLeft = false; }
-        if(rotRight) { out -= angularSpeed; rotRight = false; }
+        if (this->move_state.test(static_cast<int>(move_sense::rotLeft))) {
+            out += angularSpeed;
+            this->move_state.set(static_cast<int>(move_sense::rotLeft), false);
+        }
+        if (this->move_state.test(static_cast<int>(move_sense::rotRight))) {
+            out -= angularSpeed;
+            this->move_state.set(static_cast<int>(move_sense::rotRight), false);
+        }
         return out;
     }
 
     // true if any part is set to true
-    bool isActivelyMoving()
-    {
-        return forward || backward || left || right || up || down ||
-        rotUp || rotDown || rotLeft || rotRight ||
-        zoomIn || zoomOut;
-    }
+    bool isActivelyMoving() { return this->move_state.any(); }
 
 private:
     static constexpr float3 UP      = {0.0f,  1.0f, 0.0f};
@@ -104,17 +137,5 @@ private:
     static constexpr float3 FORWARD = {0.0f, 0.0f,  1.0f};
     static constexpr float3 BACK    = {0.0f, 0.0f, -1.0f};
 
-    // Current key states
-    bool forward = false;
-    bool backward = false;
-    bool left = false;
-    bool right = false;
-    bool up = false;
-    bool down = false;
-    bool rotUp = false;
-    bool rotDown = false;
-    bool rotLeft = false;
-    bool rotRight = false;
-    bool zoomIn = false;
-    bool zoomOut = false;
+    std::bitset<12> move_state;
 };
