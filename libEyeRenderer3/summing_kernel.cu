7dd9352bee57c9c86fbdb007f91f0be4808da866
Seb James <seb@opteran.com>
2024-11-19 11:57:14 +0000

    Simplification of the code

--- libEyeRenderer3/summing_kernel.cu
+++ libEyeRenderer3/summing_kernel.cu
@@ -117,44 +117,56 @@ inline void gpuAssert (cudaError_t code, const char *file, int line, bool abort=
    } // else { std::cout << "CUDA_SUM: " << "GPU Success: code was " << code << " (cudaSuccess)\n"; }
 }
 
-// in points to device memory containing n_arrays of n_elements float3s (RGB
-// triplets). These are arranged one-after-the other. This host function launches
-// kernels to compute the mean of each array and places these means in d_final, which
-// should be n_arrays in length.
-__host__ void shufflesum_arrays (float3* in, int n_arrays, int n_elements, float3* d_final)
-{
-    // the constexpr threadsperblock gives an ideal size (512). warp size is 32. I can't
-    // mix memory values from different arrays in a threadblock, so the threadblock has
-    // to be 1D and thus configurable in size - dynamically sized to match the number of
-    // elements in the array (n_elements - i.e. n_samples - may be <512)
-    int warps_base = n_elements / 32;
-    int warps_extra = n_elements % 32;
-    int tbx = (warps_base * 32) + (warps_extra ? 32 : 0);
-    tbx = std::min (tbx, threadsperblock);
-    dim3 stg1_blockdim(tbx, 1);
-
-    // Then figure out how many threadblocks to run.
-    dim3 stg1_griddim(1, 1);
-    // We will combine n_sums before warping
-    // int n_sums = n_elements / stg1_blockdim.x + (n_elements % stg1_blockdim.x ? 1 : 0);
-    //stg1_griddim.x = n_sums; // but leave this at 1
-    stg1_griddim.y = n_arrays / stg1_blockdim.y + (n_arrays % stg1_blockdim.y ? 1 : 0);
-
-#ifdef DEBUG_CUDA_SUM
-    std::cout << "CUDA_SUM: About to run with stg1_griddim = (" << stg1_griddim.x << " x " << stg1_griddim.y
-              << ") and stg1_blockdim = (" << stg1_blockdim.x << " x " << stg1_blockdim.y << ") thread blocks\n";
-#endif
-    reduceit_arrays<<<stg1_griddim, stg1_blockdim>>>(in, d_final, n_arrays, n_elements);
-    gpuErrchk(cudaDeviceSynchronize());
-}
-
-__host__ void
-summing_kernel (float3* d_omm, float3* d_avg, int n_pixels, int n_samples)
+/*
+ * Perform a sum of n_pixels, each containing n_samples float3-sized data points,
+ * arranged in the device memory pointed to by d_omm.
+ *
+ * The samples of one pixel (aka ommatidium) are dispersed across the memory. Sample 0
+ * of pixel 0 is at memory location 0; sample 1 of pixel 0 is at memory location (0 +
+ * n_samples) and so on. The kernel (and/or the choice of thread blocks) needs to
+ * account for this.
+ *
+ * Note that I did't figure out that the samples of one pixel/ommatidium are dispersed
+ * across the memory until later in development. It may be more efficient to re-design
+ * the choice of threadblocks (using the x dimension to refer to array and y to refer to
+ * sample) and avoid the need for tidx->midx translation in reduceit_arrays. However, as
+ * this function is not a performance bottleneck, I'll leave it for now.
+ *
+ * The result is written to d_avg, which should be a region of device memory of size
+ * n_arrays * sizeof(float3).
+ *
+ * This host function determines threadblock size/grid size and then launches the
+ * reduceit_arrays kernel to compute the sum of each array.
+ */
+__host__ void summing_kernel (float3* d_omm, float3* d_sums, int n_pixels, int n_samples)
 {
 #ifdef DEBUG_CUDA_SUM
     std::cout << "CUDA_SUM: " << __func__ << " called for n_pixels = " << n_pixels
               << " and n_samples = " << n_samples << std::endl;
 #endif
-    if (d_omm == nullptr || d_avg == nullptr) { return; }
-    shufflesum_arrays (d_omm, n_pixels, n_samples, d_avg);
+    if (d_omm == nullptr || d_sums == nullptr) { return; }
+
+    // Determine threadblock size.
+    //
+    // The constexpr threadsperblock gives an ideal block size (512). The warp size is
+    // 32. I can't mix memory values from different arrays in a threadblock, so the
+    // threadblock has to be 1D and thus configurable in size - dynamically sized to
+    // match the number of samples per pixel (n_samples - may be <512)
+    int warps_base = n_samples / 32;
+    int warps_extra = n_samples % 32;
+    int tbx = (warps_base * 32) + (warps_extra ? 32 : 0);
+    tbx = std::min (tbx, threadsperblock);
+    dim3 blockdim(tbx, 1);
+
+    // Figure out how many threadblocks to run in the threadblock grid
+    dim3 griddim(1, 1);
+    // Leave griddim.x = 1 and update griddim.y:
+    griddim.y = n_pixels / blockdim.y + (n_pixels % blockdim.y ? 1 : 0);
+
+#ifdef DEBUG_CUDA_SUM
+    std::cout << "CUDA_SUM: About to run reduceit_arrays with griddim = (" << griddim.x << " x " << griddim.y
+              << ") and blockdim = (" << blockdim.x << " x " << blockdim.y << ") thread blocks\n";
+#endif
+    reduceit_arrays<<<griddim, blockdim>>>(d_omm, d_sums, n_pixels, n_samples);
+    gpuErrchk (cudaDeviceSynchronize());
 }
--- libEyeRenderer3/summing_kernel.h
+++ libEyeRenderer3/summing_kernel.h
@@ -6,4 +6,4 @@
 /*
  * A CUDA kernel to sum up an array of arrays using the warp-shuffle method
  */
-__host__ void summing_kernel (float3* d_omm, float3* d_avg, int n_pixels, int n_samples_per_pixel);
+__host__ void summing_kernel (float3* d_omm, float3* d_sums, int n_pixels, int n_samples_per_pixel);
